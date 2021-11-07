882d1e67fbf5a91882da18795a75b99a3c6cbf90
Bren Bailey <bren7473@gmail.com>
2021-11-07 15:14:33 -0500

    Type converters set up for officials by address Room successfully persists data when interent is unavailable!

--- app/src/main/java/com/example/voterinformation/data/Constants.kt
+++ app/src/main/java/com/example/voterinformation/data/Constants.kt
@@ -2,5 +2,5 @@ package com.example.voterinformation.data
 
 object Constants {
     const val CIVICS_API_URL = "https://www.googleapis.com/"
-    const val USER_LOWER_BODY = "/civicinfo/v2/representatives?address=148 S Poplar st, athens, ga&key=AIzaSyAbtPNve1pwCI126oimKzKXAzdK8lw8u3g"
+    const val USER_LOWER_BODY = "/civicinfo/v2/representatives?address=148 S Poplar st, athens, ga&key=" + Ignored.civicsPass
 }
